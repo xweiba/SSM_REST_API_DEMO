95f3115e84597dd18202ffe18e1ac2ba37b1dec5
xweiba <lh1738289913@gmail.com>
2018-05-01 18:05:54 +0800

    拦截登陆demo完成

--- src/main/java/com/jnshu/controller/LoginController.java
+++ src/main/java/com/jnshu/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.jnshu.controller;
 
+import com.jnshu.model.Auth;
+import com.jnshu.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,16 +13,17 @@ import javax.servlet.http.HttpSession;
 /* 认证Controller */
 @Controller
 public class LoginController {
+    //日志
+    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @Autowired
+    UserService userService;
     //登陆
     @RequestMapping("/login.action")
-    public String login(HttpSession session,String username,String password){
-
-        //调用service进行用户身份认证
-
-
-        //在session中保存用户身份信息
-        session.setAttribute("username", username);
-
+    public String login(HttpSession session, Auth auth) throws Exception {
+        if(userService.findAuth(auth)){
+            //在session中保存用户身份信息
+            session.setAttribute("username", auth.getUsername());
+        }
         //重定向到用户信息页面
         return "redirect:/userList.action";
     }
