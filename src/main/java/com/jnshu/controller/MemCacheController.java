b95bfeeab17237b3ec41d02443415ba56e379c7d
xweiba <lh1738289913@gmail.com>
2018-05-20 00:45:48 +0800

    添加 Memcache api接口

--- src/main/java/com/jnshu/controller/MemCacheController.java
+++ src/main/java/com/jnshu/controller/MemCacheController.java
@@ -1,36 +1,107 @@
 package com.jnshu.controller;
 
+import com.jnshu.model.UserCustom;
+import com.jnshu.service.UserService;
 import com.jnshu.tools.MemcacheUtils;
 import com.whalin.MemCached.MemCachedClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
  * @program: taskTwo
- * @description: MemCache 测试类
+ * @description: MemCache 缓存接口
  * @author: Mr.xweiba
  * @create: 2018-05-19 00:06
  **/
 
 @Controller
+@RequestMapping("/memcache")
 public class MemCacheController {
+    @Autowired
+    UserService userService;
     private static Logger logger = LoggerFactory.getLogger(MemCacheController.class);
 
-    @RequestMapping(value = "/findSessionByKey", method = {RequestMethod.GET,RequestMethod.POST})
+    /**
+    * @Description: 获取key为id的user缓存
+    * @Param: [key]
+    * @return: java.lang.Object
+    * @Author: Mr.Wang
+    * @Date: 2018/5/19
+    */
+    @RequestMapping(value = "/api/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public Object findByKey(@RequestBody String key){
-        System.out.println(" MemcacheUtils 测试: " + key );
-        logger.info("MemCacheController.findByKey param:key="+key);
+    public Object findByKey(@RequestBody @PathVariable("id") String key){
         if(StringUtils.isEmpty(key)){
             return "key must not be empty or null!";
         }
-        return MemcacheUtils.get(key);
+        return MemcacheUtils.get("user" + key);
+    }
+
+    /**
+    * @Description: 缓存更新接口 当key不存在时取消更新
+    * @Param: [key, userCustom] 键. 值
+    * @return: boolean
+    * @Author: Mr.Wang
+    * @Date: 2018/5/19
+    */
+    @RequestMapping(value = "/api/{id}", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public boolean updateByKey(@PathVariable("id") String key, @RequestBody UserCustom userCustom){
+        userCustom.setId(Integer.valueOf(key));
+        if(StringUtils.isEmpty(key)){
+            return false;
+        }
+        return MemcacheUtils.replace("user" + key, userCustom);
+    }
+    
+    /** 
+    * @Description: 增加缓存数据 当键存在时取消存入
+    * @Param: [key, userCustom] 键, 值 
+    * @return: java.lang.Boolean 
+    * @Author: Mr.Wang 
+    * @Date: 2018/5/19 
+    */
+    @RequestMapping(value = "/api/{id}", method = RequestMethod.PUT, produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public Boolean insert(@PathVariable("id") String key, @RequestBody UserCustom userCustom){
+        System.out.println(userCustom.toString());
+        userCustom.setId(Integer.valueOf(key));
+        if(StringUtils.isEmpty(key)){
+            return false;
+        }
+        return MemcacheUtils.add("user" + key, userCustom);
+    }
+    
+    /** 
+    * @Description: 删除指定key 
+    * @Param: [key] 
+    * @return: java.lang.Boolean 
+    * @Author: Mr.Wang 
+    * @Date: 2018/5/19 
+    */ 
+    @RequestMapping(value = "/api/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public Boolean deleteByKey(@PathVariable("id") String key){
+        if(StringUtils.isEmpty(key)){
+            return false;
+        }
+        return MemcacheUtils.delete("user" + key);
+    }
+    /**
+     * @Description: 清除缓存中的所有键值对
+     * @Param: []
+     * @return: boolean
+     * @Author: Mr.Wang
+     * @Date: 2018/5/19
+     */
+    @RequestMapping(value = "/api/all", method = RequestMethod.DELETE)
+    @ResponseBody
+    public boolean flashAll() throws Exception {
+        return MemcacheUtils.flashAll();
     }
 }
